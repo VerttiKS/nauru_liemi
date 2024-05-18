536cb20bcbeeffe3c7ee4ebd2623657afa1732a6
Vertti <122641331+VerttiKS@users.noreply.github.com>
2024-05-19 01:06:57 +0300

    Little patch
    -no more outline on buttons
    -start comic is skippable by pressing space
    -start comic lasts 8 seconds instead of 6

--- scenes/scripts/comic.gd
+++ scenes/scripts/comic.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-
+func _process(delta):
+	if Input.is_action_just_pressed("jump"):
+		get_tree().change_scene_to_file("res://scenes/e_1m_1.tscn") #Pressing space let's you skip the comic
 
 func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/e_1m_1.tscn")
